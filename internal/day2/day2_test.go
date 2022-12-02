291b0b34d7d3db64eb25e5c6c0450a240338b17a
tomwatson6 <tom.watson6@outlook.com>
2022-12-02 11:10:10 +0000

    update names of testing for day 2

--- internal/day2/day2_test.go
+++ internal/day2/day2_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/tomwatson6/AdventOfCode2022_Go/internal/day2"
 )
 
-func TestPart1(t *testing.T) {
+func TestD2P1(t *testing.T) {
 	score, err := day2.Part1("simple_input.txt")
 	if err != nil {
 		t.Fatal(err)
@@ -19,7 +19,7 @@ func TestPart1(t *testing.T) {
 	}
 }
 
-func TestPart2(t *testing.T) {
+func TestD2P2(t *testing.T) {
 	score, err := day2.Part2("simple_input.txt")
 	if err != nil {
 		t.Fatal(err)
@@ -32,7 +32,7 @@ func TestPart2(t *testing.T) {
 	}
 }
 
-func BenchmarkPart1(b *testing.B) {
+func BenchmarkD2P1(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_, err := day2.Part1("simple_input.txt")
 		if err != nil {
@@ -41,7 +41,7 @@ func BenchmarkPart1(b *testing.B) {
 	}
 }
 
-func BenchmarkPart2(b *testing.B) {
+func BenchmarkD2P2(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_, err := day2.Part2("simple_input.txt")
 		if err != nil {
